5adc27d7045cefd872bdf13b17e69087f4582e9b
Paul Grave <paul@stomer.net>
2011-11-26 23:35:50 +0000

    changed: fully functioning

--- parseWeather.py
+++ parseWeather.py
@@ -3,21 +3,23 @@
 import xml.parsers.expat
 import urllib
 import time
-from datetime import datetime
+import copy
+from datetime import datetime, timedelta
 
 class YrCyclingWeather:
 	yrForecastURL = ""
 	forecastInfo = {}
 	hoursOfInterest = []
-	forecastObject = {'windSpeed': 0, 'windDescription': '', 'windDirection': '', 'precipitation': 0, 'temperature': 0, 'description': ''}
+	emptyForecastObject = {'windSpeed': 0, 'windDescription': '', 'windDirection': '', 'precipitation': 0, 'temperature': 0, 'description': ''}
 	currentFromDatetime = ""
-	humanReadableDescription = "{0}: {o[description]}. Wind: {o[windDescription]} {o[windSpeed]} {o[windDirection]}, Precipitation: {o[precipitation]}, Temp: {o[temperature]}"
+	currentForecastObject = ""
+	humanReadableDescription = "{0} {1} - {o[description]}. Wind: {o[windDescription]} {o[windSpeed]} {o[windDirection]}, Precipitation: {o[precipitation]}, Temp: {o[temperature]}"
 	
 	def __init__(self, yrForecastURL):
 		self.yrForecastURL = yrForecastURL
 
-	def addHourOfInterest(self, datetimeString):
-		self.hoursOfInterest.append(datetimeString)
+	def addHourOfInterest(self, hour):
+		self.hoursOfInterest.append(hour)
 
 	def createForecastText(self):
 		fh = urllib.urlopen(self.yrForecastURL)
@@ -31,45 +33,59 @@ class YrCyclingWeather:
 		p.Parse(forecastXML, 1)
 				
 		forecastStrings = []
-		for datetimeString, forecastObject in self.forecastInfo.iteritems():
+		for n in range(0, 2):
+			(datetimeString, forecastObject) = self.forecastInfo.popitem()
 			dt = datetime.strptime(datetimeString, "%Y-%m-%dT%H:%M:%S")
-			forecastStrings.append(self.humanReadableDescription.format(dt.strftime('%H:%M'), o=forecastObject))
+			
+			now = datetime.now()
+			day = dt.strftime('%d/%m')
+			
+			if now.strftime('%d') == dt.strftime('%d'):
+				day = 'Today'
+			
+			if (now +  timedelta(days=1)).strftime('%d') == dt.strftime('%d'):
+				day = 'Tomorrow'
+			
+			forecastStrings.append(self.humanReadableDescription.format(day, dt.strftime('%H:%M'), o=forecastObject))
 		
 		return "\n".join(forecastStrings)
 				
 	def start_element(self, name, attrs):
 		if name == "time":	
 			self.currentFromDatetime = attrs['from']
-			
 			try:
-				if self.hoursOfInterest.index(self.currentFromDatetime) != -1:
-					self.forecastInfo[self.currentFromDatetime] = self.forecastObject
+				if self.hoursOfInterest.index(datetime.strptime(self.currentFromDatetime, "%Y-%m-%dT%H:%M:%S").hour) != -1:
+					self.currentForecastObject = copy.copy(self.emptyForecastObject)
+				else:
+					self.currentForecastObject = ""
 			except ValueError:
 				pass
 
 			return
 			
 		try:
-			if  self.hoursOfInterest.index(self.currentFromDatetime) != -1:
+			if self.hoursOfInterest.index(datetime.strptime(self.currentFromDatetime, "%Y-%m-%dT%H:%M:%S").hour) != -1:
 				if name == "symbol":
-					self.forecastInfo[self.currentFromDatetime]['description'] = attrs['name']
+					self.currentForecastObject['description'] = attrs['name']
 				if name == "windDirection":
-					self.forecastInfo[self.currentFromDatetime]['windDirection'] = attrs['code']
+					self.currentForecastObject['windDirection'] = attrs['code']
 				if name == "windSpeed":
-					self.forecastInfo[self.currentFromDatetime]['windSpeed'] = 3.6 * float(attrs['mps'])
-					self.forecastInfo[self.currentFromDatetime]['windDescription'] = attrs['name']
+					self.currentForecastObject['windSpeed'] = 3.6 * float(attrs['mps'])
+					self.currentForecastObject['windDescription'] = attrs['name']
 				if name == "temperature":
-					self.forecastInfo[self.currentFromDatetime]['temperature'] = attrs['value']
+					self.currentForecastObject['temperature'] = attrs['value']
 		except ValueError:
 			pass
 
 	def end_element(self, name):
-		if name == "time":
+		if name == "time" and self.currentForecastObject != "":
+			self.forecastInfo[self.currentFromDatetime] = self.currentForecastObject;
+			self.currentForecastObject = ""
 			self.currentFromDatetime = ""
 		
 		
 yrForecast = YrCyclingWeather("http://www.yr.no/place/United_Kingdom/England/Pudsey~2639866/forecast_hour_by_hour.xml")
-yrForecast.addHourOfInterest(time.strftime('%Y-%m-%dT08:00:00'))
-yrForecast.addHourOfInterest(time.strftime('%Y-%m-%dT17:00:00'))
+yrForecast.addHourOfInterest(8)
+yrForecast.addHourOfInterest(17)
 
 print yrForecast.createForecastText()
