330b39c9eb7a91f1f5934abc3cbcfe1df860975d
Paul Grave <paul@stomer.net>
2011-11-26 17:10:37 +0000

    refactored to a class

--- parseWeather.py
+++ parseWeather.py
@@ -3,38 +3,73 @@
 import xml.parsers.expat
 import urllib
 import time
+from datetime import datetime
 
-fh = urllib.urlopen("http://www.yr.no/place/United_Kingdom/England/Pudsey~2639866/forecast_hour_by_hour.xml")
-forecastXML = fh.read()
-toWorkDateTime = time.strftime('%Y-%m-%sT08:00:00')
-toHomeDateTime = time.strftime('%Y-%m-%sT17:00:00')
-
-forecastInfo[toWorkForecastInfo] = forecastInfo[toWorkDateTime] = {'windSpeed': 0, 'windDescription': '', 'windDirection': '', 'precipitation': 0, 'temperature': 0, 'description': ''}
-
-
-toWorkObject = { }
-# 3 handler functions
-def start_element(name, attrs):
-	if name == "time":	
-	    currentFromDatetime = attrs['from']
-	    return
-
-	if currentFromDatetime == toWorkDateTime:
-		if name == "symbol":
-			toWorkForecastInfo['description'] = attrs['name']
+class YrCyclingWeather:
+	yrForecastURL = ""
+	forecastInfo = {}
+	hoursOfInterest = []
+	forecastObject = {'windSpeed': 0, 'windDescription': '', 'windDirection': '', 'precipitation': 0, 'temperature': 0, 'description': ''}
+	currentFromDatetime = ""
+	humanReadableDescription = "{0}: {o[description]}. Wind: {o[windDescription]} {o[windSpeed]} {o[windDirection]}, Precipitation: {o[precipitation]}, Temp: {o[temperature]}"
 	
-def end_element(name):
-    print 'End element:', name
-def char_data(data):
-    print 'Character data:', repr(data)
+	def __init__(self, yrForecastURL):
+		self.yrForecastURL = yrForecastURL
 
-p = xml.parsers.expat.ParserCreate()
+	def addHourOfInterest(self, datetimeString):
+		self.hoursOfInterest.append(datetimeString)
 
-p.StartElementHandler = start_element
-p.EndElementHandler = end_element
-p.CharacterDataHandler = char_data
+	def createForecastText(self):
+		fh = urllib.urlopen(self.yrForecastURL)
+		forecastXML = fh.read()
+		
+		p = xml.parsers.expat.ParserCreate()
 
-p.Parse(forecastXML, 1)
+		p.StartElementHandler = self.start_element
+ 		p.EndElementHandler = self.end_element
+# 		p.CharacterDataHandler = self.char_data
+		p.Parse(forecastXML, 1)
+				
+		forecastStrings = []
+		for datetimeString, forecastObject in self.forecastInfo.iteritems():
+			dt = datetime.strptime(datetimeString, "%Y-%m-%dT%H:%M:%S")
+			forecastStrings.append(self.humanReadableDescription.format(dt.strftime('%H:%M'), o=forecastObject))
+		
+		return "\n".join(forecastStrings)
+				
+	def start_element(self, name, attrs):
+		if name == "time":	
+			self.currentFromDatetime = attrs['from']
+			
+			try:
+				if self.hoursOfInterest.index(self.currentFromDatetime) != -1:
+					self.forecastInfo[self.currentFromDatetime] = self.forecastObject
+			except ValueError:
+				pass
 
-print toWorkForecastInfo
-print toHomeForecastInfo
+			return
+			
+		try:
+			if  self.hoursOfInterest.index(self.currentFromDatetime) != -1:
+				if name == "symbol":
+					self.forecastInfo[self.currentFromDatetime]['description'] = attrs['name']
+				if name == "windDirection":
+					self.forecastInfo[self.currentFromDatetime]['windDirection'] = attrs['code']
+				if name == "windSpeed":
+					self.forecastInfo[self.currentFromDatetime]['windSpeed'] = 3.6 * float(attrs['mps'])
+					self.forecastInfo[self.currentFromDatetime]['windDescription'] = attrs['name']
+				if name == "temperature":
+					self.forecastInfo[self.currentFromDatetime]['temperature'] = attrs['value']
+		except ValueError:
+			pass
+
+	def end_element(self, name):
+		if name == "time":
+			self.currentFromDatetime = ""
+		
+		
+yrForecast = YrCyclingWeather("http://www.yr.no/place/United_Kingdom/England/Pudsey~2639866/forecast_hour_by_hour.xml")
+yrForecast.addHourOfInterest(time.strftime('%Y-%m-%dT08:00:00'))
+yrForecast.addHourOfInterest(time.strftime('%Y-%m-%dT17:00:00'))
+
+print yrForecast.createForecastText()
